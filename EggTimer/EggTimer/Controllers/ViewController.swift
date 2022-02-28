9083633c281504d24b650a3c5cfe7d7d98900a44
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-02-28 14:52:23 +0300

    Dic, Switch, Optinals, Timer

---
+++ EggTimer/EggTimer/Controllers/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var doneLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var counter = 0
+    var timer = Timer()
+    var progressCounter: Float = 0.0
+    var alarm: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            doneLabel.text = "\(counter) seconds left."
+            counter -= 1
+            progressBar.progress += progressCounter
+
+        }else if counter == 0 {
+            timer.invalidate()
+            doneLabel.text = "Done!"
+            playAlarm()
+        }
+    }
+    
+    let eggsCookingSeconds = ["Soft" : 3, "Medium" : 420, "Hard" : 720]
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        progressBar.progress = 0.0
+        counter = eggsCookingSeconds[sender.currentTitle!]!
+        progressCounter = 1.0 / Float(counter)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+
+    func playAlarm() {
+            
+            let soundURL = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                    
+                    do {
+                        alarm = try AVAudioPlayer(contentsOf: soundURL!)
+                    }catch{
+                        print(error)
+                    }
+                    alarm.play()
+            
+        }
+    
+
+}
